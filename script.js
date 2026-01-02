64660a12d4615e7bc0d7b5e9333a2882a80534ab
ahmedamir <ae4292482@gmail.com>
2026-01-02 18:43:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simp// Simple welcome animation log
+window.onload = () => {
+    console.log("Welcome to Ahmed Elamir Website 🚀");
+};
+le welcome animation log
+window.onload = () => {
+    console.log("Welcome to Ahmed Elamir Website 🚀");
+};
+v
